9c75dae041b7fec60f7b9366f906fed8ff48b62d
Pallishree <pallishreebehera01@gmail.com>
2026-02-08 21:17:46 +0530

    new changes

--- src/utils/uploadToGcp.ts
+++ src/utils/uploadToGcp.ts
@@ -1,6 +1,33 @@
 import { bucket } from "../config/gcp";
 import { v4 as uuid } from "uuid";
 
+// export const uploadImageToGCP = async (
+//   file: Express.Multer.File,
+//   folder: string,
+// ) => {
+//   const fileName = `${folder}/${uuid()}-${file.originalname}`;
+//   const blob = bucket.file(fileName);
+
+//   const blobStream = blob.createWriteStream({
+//     resumable: false,
+//     contentType: file.mimetype,
+//   });
+
+//   return new Promise<string>((resolve, reject) => {
+//     blobStream.on("error", reject);
+
+//     blobStream.on("finish", async () => {
+//       // Make file public (OPTION 1)
+//       await blob.makePublic();
+
+//       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
+//       resolve(publicUrl);
+//     });
+
+//     blobStream.end(file.buffer);
+//   });
+// };
+
 export const uploadImageToGCP = async (
   file: Express.Multer.File,
   folder: string,
@@ -16,10 +43,7 @@ export const uploadImageToGCP = async (
   return new Promise<string>((resolve, reject) => {
     blobStream.on("error", reject);
 
-    blobStream.on("finish", async () => {
-      // Make file public (OPTION 1)
-      await blob.makePublic();
-
+    blobStream.on("finish", () => {
       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
       resolve(publicUrl);
     });
@@ -27,3 +51,4 @@ export const uploadImageToGCP = async (
     blobStream.end(file.buffer);
   });
 };
+
